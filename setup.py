7b74fbb9c4ac9baaa9ae2e393c4bf3ee2452d14d
creuter720 <jinnang233@outlook.com>
2022-08-21 14:05:43 +0800

    Remove setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup,find_packages
-
-setup(name="signEverywhere",
-        version="0.1",
-        description="Sign and verify file everywhere",
-        packages=find_packages(),
-        install_requires=["argon2","cffi","pycparser","PySPX"],
-        )
