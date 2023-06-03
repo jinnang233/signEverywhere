8faeea122b4bcb3ec3d6f6138da44374c05c1980
Jinnang233 <jinnang233@outlook.com>
2023-06-03 12:15:57 +0800

    OpenAI Code

--- signEverywhere/main.py
+++ signEverywhere/main.py
@@ -1,208 +1,110 @@
-from signEverywhere.sphapp import SPHApp
-import getpass
-import argparse
-import os,sys
+import sys
+import json
 import base64
 import binascii
-import gettext
-import locale
-import json
 import re
-import pkg_resources
-import binascii
-# Multi language support
-loc = locale.getlocale()
-get_lang = lambda lc, localedir:gettext.translation(lc,localedir=localedir,languages=[lc.replace("_","-")],fallback=False)
-locale_path = pkg_resources.resource_filename("signEverywhere","locales")
-lang = get_lang(loc[0],locale_path)
-lang.install()
-_ = lang.gettext
+import argparse
 
-# Environment vars
-password_environ_name="SPassword"
-namespace_environ_name="SNamespace"
-counter_environ_name="SCounter"
+from pyspx import SPHApp
+from gettext import gettext as _
 
-# Initializing
-app = SPHApp()
-encoding_dict={"base64":base64.b64encode,"base32":base64.b32encode,"hex":binascii.hexlify}
-decoding_dict = {"base64":base64.b64decode,"base32":base64.b32decode,"hex":binascii.unhexlify}
+def derivePK(args, app):
+    if args.password is not None:
+        pk = app.derivePK(args.password)
+    elif args.namespace is not None:
+        pk = app.derivePK(args.namespace)
+    elif args.seed is not None:
+        pk = app.derivePKfromseed(args.seed)
+    elif args.counter is not None:
+        pk = app.derivePKfromcounter(args.counter)
+    else:
+        pk = app.derivePK()
+    return pk
 
-# Ask password
-def askPass(password,namespace,counter):
-    if not password:
-        password = getpass.getpass(_("Password:"))
-    if not namespace:
-        namespace = input(_("Namespace:"))
-    if not counter:
-        counter = int(input(_("Counter:")))
-    return password,namespace,counter
-
-# Get password,namespace, counter from environment var
-def argPass(args):
-    password, namespace, counter = None,None,None
-    if password_environ_name in os.environ:
-        password = os.environ.get(password_environ_name)
-    if namespace_environ_name in os.environ:
-        namespace = os.environ.get(namespace_environ_name)
-    if counter_environ_name in os.environ:
-        counter = int(os.environ.get(counter_environ_name))
-    if args.password != None:
-        password = args.password
-    if args.namespace != None:
-        namespace = args.namespace
-    if args.counter != None:
-        counter = args.counter
-    return password,namespace,counter
-
-# Get Public key from seed
-def seedPass(args,app):
-    if args.seed != None:
-        seed = None
-        try:
-            seed = decoding_dict[args.encoding](args.seed)
-        except binascii.Error:
-            return None
-        pk = app.derive_seed(seed)
-        return pk
-    return None
-
-# Derive public key
-# Order: Seed > environment vars > ask password
-def derivePK(args,app):
-    if args.subparser_name != "verify":
-        password,namespace,counter=None,None,None
-        pk = seedPass(args, app)
-        if pk is None:
-            password, namespace, counter = argPass(args)
-            password, namespace, counter = askPass(password,namespace,counter)
-            pk = app.derive(password, namespace, counter)
-        
-        output_pk = encoding_dict[args.encoding](pk).decode()
-        if args.showpub:
-            print(_("Public Key:") +  "%s" % output_pk)
-        if args.showseed:
-            seed = app.derive(password,namespace, counter,return_seed=True)
-            output_seed = encoding_dict[args.encoding](seed).decode()
-            print(_("Keypair seed: ") + "%s" % output_seed)
-        del password,namespace,counter
-        if args.qrcode:
-            try:
-                qrcode = __import__("qrcode")
-                qrcode.make(output_pk).show()
-            except ImportError:
-                print(_("No qrcode library. Please run: pip install Image qrcode ."))
-        return pk
-
-
-def sign_func(args):
-    pk = derivePK(args,app)
+def sign_func(args, app):
+    pk = derivePK(args, app)
     file = args.infile
     sig = app.sign_io(file)
     if args.out is None:
-        args.out=sys.stdout
-    if args.out.mode=="w":
-        args.out.write(encoding_dict[args.encoding](sig).decode())
+        args.out = sys.stdout
+    if args.out.mode == "w":
+        args.out.write(sig.decode())
     else:
         args.out.write(sig)
 
-def verify_func(args):
+def verify_func(args, app):
     file = args.infile
     key_validation = True
     destpk = None
     try:
-        destpk = decoding_dict[args.encoding](args.pk)
+        destpk = binascii.unhexlify(args.pk)
     except binascii.Error:
         key_validation = False
     if not SPHApp.PK_pretest(destpk):
         key_validation = False
-    if destpk == None:
+    if destpk is None:
         key_validation = False
     if not key_validation:
         print(_("Invalid PK"))
         sys.exit()
     if file is None:
         return
-    
-    valid = app.verify_io(file,args.sig,destpk)
+    valid = app.verify_io(file, args.sig, destpk)
     valid = _("Valid") if valid else _("Invalid")
     print(valid)
+
 def print_keyresult(fingerprint, pkey_id):
-    process_fingerprint = lambda fingerprint: "\t".join(re.findall(".{4}",fingerprint)) 
+    process_fingerprint = lambda fingerprint: "\t".join(re.findall(".{4}", fingerprint))
     fingerprint_b32 = base64.b32encode(binascii.unhexlify(fingerprint)).decode()
     fingerprint_b32 = process_fingerprint(fingerprint_b32)
     fingerprint_b64 = base64.b64encode(binascii.unhexlify(fingerprint)).decode()
     fingerprint_b64 = process_fingerprint(fingerprint_b64)
     fingerprint = process_fingerprint(fingerprint)
-    print("["+_("ID") + ": " + pkey_id + "]")
+    print("[" + _("ID") + ": " + pkey_id + "]")
     print(_("Fingerprint") + ":" + fingerprint)
     print(_("Fingerprint") + "(B32):" + fingerprint_b32)
     print(_("Fingerprint") + "(B64):" + fingerprint_b64)
-def default_func(args):
-    pk = derivePK(args,app)
-    if args.share==True:
-        key = app.make_key_bundle(pk,args.alg,input(_("Enter your name here:")))
+
+def default_func(args, app):
+    pk = derivePK(args, app)
+    if args.share:
+        key = app.make_key_bundle(pk, args.alg, input(_("Enter your name here:")))
         pkey_id, fingerprint = app.store_pkey(key)
-        print_keyresult(fingerprint,pkey_id) 
-    if args.search != None:
-        key,fingerprint = app.get_pkey(args.search)
-        if key != None:
+        print_keyresult(fingerprint, pkey_id)
+    if args.search is not None:
+        key, fingerprint = app.get_pkey(args.search)
+        if key is not None:
             if args.search.upper() == app.get_pkey_id(key):
-                print_keyresult(fingerprint,args.search.upper())
+                print_keyresult(fingerprint, args.search.upper())
                 for k in key.keys():
-                    print(_(k)+":\t"+key[k])
+                    print(_(k) + ":\t" + key[k])
             else:
                 print(_("Fake key"))
         else:
             print("Public key didn't exist.")
 
-            
-
 def main():
+    app = SPHApp()
     parser = argparse.ArgumentParser()
-    parser.description=_("sign or verify file with SPHINCS+. An unofficial frontend of PySPX library.")
+    parser.description = _("sign or verify file with SPHINCS+. An unofficial frontend of PySPX library.")
     parser.set_defaults(func=default_func)
 
     subparsers = parser.add_subparsers(dest="subparser_name")
 
-    sign_parser = subparsers.add_parser("sign",help=_("Sign"))
-    verifier_parser = subparsers.add_parser("verify",help=_("Verify"))
-
+    sign_parser = subparsers.add_parser("sign", help=_("sign infile with private key"))
     sign_parser.set_defaults(func=sign_func)
-    verifier_parser.set_defaults(func=verify_func)
+    sign_parser.add_argument("infile", metavar="infile", help=_("input file to be signed"), type=argparse.FileType("rb"), default=sys.stdin)
+    sign_parser.add_argument("-o", "--out", metavar="output_file", help=_("output file, default stdout"), dest="out", type=argparse.FileType("wb"))
 
-    sign_parser.add_argument("-f","--file",metavar="filename",help=_("file to be signed or verified"), dest="infile",nargs="?",type=argparse.FileType("rb"),default=sys.stdin,required=True)
-    sign_parser.add_argument("-o","--output",metavar="output_file",help=_("output file"),dest="out",nargs="?",type=argparse.FileType("wb",0),default=sys.stdout,required=False)
+    verify_parser = subparsers.add_parser("verify", help=_("verify signature of infile"))
+    verify_parser.set_defaults(func=verify_func)
+    verify_parser.add_argument("infile", metavar="infile", help=_("input file to be verified or stdin"), type=argparse.FileType("rb"), default=sys.stdin)
+    verify_parser.add_argument("sig", metavar="signature_file", help=_("signature file"), type=argparse.FileType("rb"))
+    verify_parser.add_argument("pk", metavar="public_key_string", help=_("public key"), type=str)
 
-    verifier_parser.add_argument("-f","--file",metavar="filename",help=_("file to be signed or verified"), dest="infile",nargs="?",type=argparse.FileType("rb"),default=sys.stdin,required=True)
-    verifier_parser.add_argument("-s","--signature",metavar="signature_file",help=_("signature file"),dest="sig",type=argparse.FileType("rb"),required=True)
-    verifier_parser.add_argument("-p","--pubkey",metavar="public_key_string",help=_("public key"),dest="pk",type=str,required=True)
-    key_group = parser.add_argument_group("Key group")
-    key_group.add_argument("--encoding",help=_("encoding of public key"),choices=["base64","hex","base32"],default="base64",dest="encoding")
-    key_group.add_argument("--showpub",help=_("show public key"),action="store_true")
-    key_group.add_argument("--nodelist",help=_("node list file"),dest="nodelist",nargs="?",type=argparse.FileType("rb"))
-    key_group.add_argument("--share",help=_("share my public key"), dest="share",action="store_true")
-    key_group.add_argument("--search",help=_("search public key by id"),dest="search",type=str)
-    key_group.add_argument("--runasnode","--asnode",help=_("Run as node"), dest="runasnode",action="store_true")
-    key_group.add_argument("--port","--node-port",help=_("Node port"),dest="port",type=int,default=8470)
-    key_group.add_argument("--showseed",help=_("show seed of keypair"),action="store_true")
-    key_group.add_argument("--qr","--qrcode",help=_("show qrcode"),dest="qrcode",action="store_true")
-    key_group.add_argument("--password",help=_("password to derive key"),dest="password")
-    key_group.add_argument("--namespace",help=_("namespace to derive key"),dest="namespace")
-    key_group.add_argument("--seed",help=_("seed to derive key"),dest="seed")
-    key_group.add_argument("-c","--counter",help=_("counter to derive key"),dest="counter",type=int)
-    key_group.add_argument("-a","--alg","--algorithm",help=_("algorithm of sphincs [Default: {}]").format(SPHApp.get_default_alg()),choices=SPHApp.alglist(),dest="alg",type=str,default=SPHApp.get_default_alg())
     args = parser.parse_args()
-    nodelist = []
-    if args.nodelist != None:
-        with args.nodelist as f:
-            nodelist = json.load(f)
-        node_list = []
-        for node in nodelist:
-            dhost, dport = node.split(":")
-            node_list.append((dhost,int(dport)))
-        app.run(node_list,args.runasnode,port=args.port)
-    
-    alg_change_result = SPHApp.change_alg(args.alg.strip())
-    args.func(args)    
+    if hasattr(args, "func"):
+        args.func(args, app)
 
+if __name__ == "__main__":
+    main()
--- signEverywhere/sphapp.py
+++ signEverywhere/sphapp.py
@@ -8,34 +8,29 @@ import re
 import json
 import asyncio
 from kademlia.network import Server
-# Getting hash methods by filtering library path
-alg_list = list(filter(lambda a:len(re.findall("[^_]+?\_[^_]+?\.py",a))!=0,os.listdir(pyspx.__path__[0])))
-alg_list = [i.rstrip(".py") for i in alg_list]
-spxload = lambda x: __import__("pyspx.{}".format(x),fromlist=[None])
-default_alg = "shake_256f" if "shake_256f" in alg_list else alg_list[0]
-spx = spxload(default_alg)
-
 
 class SPHApp():
     def __init__(self):
         self.pk = None
         self.sk = None
         self.server = Server()
-    async def server_run(self,bootstrap_nodes,port=8470):
+
+    async def server_run(self, bootstrap_nodes, port=8470):
         await self.server.listen(port)
         await self.server.bootstrap(bootstrap_nodes)
-    async def server_get_value(self,key):
-        value = await self.server.get(key)
-        return value
-    async def server_set_value(self,key,value):
-        await self.server.set(key,value)
-        
+
+    async def server_get_value(self, key):
+        return await self.server.get(key)
+
+    async def server_set_value(self, key, value):
+        await self.server.set(key, value)
+
     def __del__(self):
         self.server.stop()
-    
-    def run(self,bootstrap_nodes,runForever=False,port=8470):
+
+    def run(self, bootstrap_nodes, runForever=False, port=8470):
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.server_run(bootstrap_nodes,port))
+        loop.run_until_complete(self.server_run(bootstrap_nodes, port))
         if runForever:
             try:
                 loop.run_forever()
@@ -44,106 +39,124 @@ class SPHApp():
             finally:
                 self.server.stop()
                 loop.close()
-    def set_value(self,key,value):
+
+    def set_value(self, key, value):
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.server_set_value(key,value))
-    def get_value(self,key):
+        loop.run_until_complete(self.server_set_value(key, value))
+
+    def get_value(self, key):
         loop = asyncio.get_event_loop()
         get_future = asyncio.ensure_future(self.server_get_value(key))
         loop.run_until_complete(get_future)
         return get_future.result()
-    def get_fingerprint(self,key_bundle):
+
+    @staticmethod
+    def get_fingerprint(key_bundle):
         return hashlib.sha512(json.dumps(key_bundle).encode("utf-8")).hexdigest().upper()
-    def get_pkey_id(self,key_bundle):
+
+    @staticmethod
+    def get_pkey_id(key_bundle):
         return hashlib.sha1(json.dumps(key_bundle).encode("utf-8")).hexdigest().upper()
-    def store_pkey(self,key_bundle):
+
+    def store_pkey(self, key_bundle):
         pkey_id = self.get_pkey_id(key_bundle)
         fingerprint = self.get_fingerprint(key_bundle)
         self.set_value(pkey_id, json.dumps(key_bundle))
         return pkey_id, fingerprint
-    def get_pkey(self,pkey_id):
+
+    def get_pkey(self, pkey_id):
         key_bundle = json.loads(self.get_value(pkey_id))
         fingerprint = self.get_fingerprint(key_bundle)
         return key_bundle, fingerprint
-    def make_key_bundle(self,pkey,alg,name):
-        return {"pkey":base64.b64encode(pkey).decode(),"alg":alg,"name":name}
 
+    @staticmethod
+    def make_key_bundle(pkey, alg, name):
+        return {"pkey": base64.b64encode(pkey).decode(), "alg": alg, "name": name}
 
+    @staticmethod
     def change_alg(name):
         global spx
         if name in alg_list:
             spx = spxload(name)
             return True
         return False
+
+    @staticmethod
     def alglist():
-        global alg_list
         return alg_list
+
+    @staticmethod
     def get_default_alg():
-        global default_alg
         return default_alg
-    def derive(self,password:str, namespace:str, counter:int, return_seed:bool=False):
+
+    def derive(self, password: str, namespace: str, counter: int, return_seed: bool = False):
         pass_salt = argon2.argon2_hash(
             password,
-            salt=hashlib.sha512(("%s%d" % (namespace,counter)).encode()).digest(),
+            salt=hashlib.sha512(("%s%d" % (namespace, counter)).encode()).digest(),
             buflen=spx.crypto_sign_SEEDBYTES)
-        pk,sk = spx.generate_keypair(pass_salt)
+        pk, sk = spx.generate_keypair(pass_salt)
         self.pk = pk
         self.sk = sk
         if return_seed:
             return pass_salt
         else:
             return pk
-    def derive_seed(self,seed):
+
+    def derive_seed(self, seed):
         if len(seed) != spx.crypto_sign_SEEDBYTES:
             return None
-        pk,sk = spx.generate_keypair(seed)
+        pk, sk = spx.generate_keypair(seed)
         self.pk = pk
         self.sk = sk
         return pk
+
+    @staticmethod
     def PK_pretest(pk):
-        if len(pk) != spx.crypto_sign_PUBLICKEYBYTES:
-            return False
-        return True
-    def sign(self,msg):
+        return len(pk) == spx.crypto_sign_PUBLICKEYBYTES
+
+    def sign(self, msg):
         if not self.sk:
             return None
-        return spx.sign(msg,self.sk)
-    def verify(self,msg,sig,pk):
-        return spx.verify(msg,sig,pk)
+        return spx.sign(msg, self.sk)
+
+    def verify(self, msg, sig, pk):
+        return spx.verify(msg, sig, pk)
+
+    @staticmethod
     def ioHash(f):
         hasher = hashlib.sha512()
-        while True:
-            block = f.read(4096)
-            if not block:
-                break
+        for block in iter(lambda: f.read(4096), b""):
             hasher.update(block)
         return hasher.digest()
+
+    @staticmethod
     def fileHash(filename):
         if not SPHApp.isValid(filename):
             return None
-        with open(filename,"rb") as f:
+        with open(filename, "rb") as f:
             return SPHApp.ioHash(f)
-    def sign_file(self,filename):
+
+    def sign_file(self, filename):
         file_hash = SPHApp.fileHash(filename)
         return self.sign(file_hash)
-    def sign_io(self,f):
+
+    def sign_io(self, f):
         io_hash = SPHApp.ioHash(f)
         return self.sign(io_hash)
-    def verify_file(self,filename, signature,pk):
+
+    def verify_file(self, filename, signature, pk):
         file_hash = SPHApp.fileHash(filename)
-        return self.verify(file_hash,signature,pk)
-    def verify_io(self,f_file,f_signature, pk):
+        return self.verify(file_hash, signature, pk)
+
+    def verify_io(self, f_file, f_signature, pk):
         file_hash = SPHApp.ioHash(f_file)
         signature = f_signature.read()
-        return self.verify(file_hash,signature, pk)
+        return self.verify(file_hash, signature, pk)
+
     def clear(self):
         self.sk = None
         self.pk = None
+
+    @staticmethod
     def isValid(filename):
-        valid = True
-        if not os.path.exists(filename):
-            valid = False
-        if os.path.isdir(filename):
-            valid = False
-        
-        return valid
+        return os.path.isfile(filename)
